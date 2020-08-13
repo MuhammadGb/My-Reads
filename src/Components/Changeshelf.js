c6db140f72034131d98c0f6110a318b5d431d9d0
Steve Jones <jones.wiredbraincoffee@gmail.com>
2020-08-13 03:35:28 +0100

    Change to Functional Component

--- src/Components/Changeshelf.js
+++ src/Components/Changeshelf.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'; 
 
-class ChangeShelf extends Component {
-    changeButton = event => {
-        this.props.selectShelf(this.props.books, event.target.value)
+function ChangeShelf (props) {
+    const changeButton = event => {
+        props.selectShelf(props.books, event.target.value)
     }
 
-    render() {
-        const {shelf, books} = this.props;
-        books.shelf = shelf;
+    //render() {
+        //const {shelf, books} = this.props;
+        props.books.shelf = props.shelf;
         
         return (
-        <select onChange={this.changeButton} defaultValue={books.shelf}>
+        <select onChange={changeButton} defaultValue={props.books.shelf}>
             <option value="move" disabled>Move to...</option>
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
@@ -18,7 +18,7 @@ class ChangeShelf extends Component {
             <option value="none">None</option>
         </select>
         )
-    }
+    //}
 }
 
 export default ChangeShelf
